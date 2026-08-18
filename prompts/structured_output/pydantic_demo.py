ca3d9dd022d438f1281b09a27f8b964a2964772a
kapoordcu <gaurav.kapoor23@outlook.com>
2026-08-19 00:56:44 +0200

    structured output: typeddict, pydantic and json schema

---
+++ prompts/structured_output/pydantic_demo.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+
+class Student(BaseModel):
+    name: str = 'nitish'
+    age: Optional[int] = None
+    cgpa: float = Field(gt=0, lt=10, default=5, description='A decimal value representing the cgpa of the student')
+
+
+new_student = {'age': '32', 'cgpa': '9'}
+
+student = Student(**new_student)
+student_dict = dict(student)
+
+print(student_dict['age'])
+
+student_json = student.model_dump_json()
+print(student_json)
